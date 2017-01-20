b3e97bc83ea41bc8e6d732a0c3ab891029b0bdb9
brandon <bcooper085@gmail.com>
2017-01-20 10:38:53 -0800

    add html form and output div to test counter function

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,19 @@
+var userInput;
+var results;
+var storage = [];
+
 function counter(num) {
   var start = 0;
   while (start <= num) {
-    storage.val(start);
+    console.log(start);
     start++;
   }
-}
 
-function converter(num) {
-  if (num % 3 === 0) {
-    num = "ping";
-  } else if (num % 5 === 0) {
-    num = "pong";
-  } else if (num % 15 === 0) {
-    num = "ping-pong"
-  }
-}
+
+$(function() {
+  $('form').submit(function() {
+    var num = $("#userInput").val();
+    // $('.output').text(results);
+    event.preventDefault();
+  });
+});
